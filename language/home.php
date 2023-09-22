3e4903e599c99ee4cfc49000e2bc1872ce1760fa
EliezerSu <eliezersuarez2000@gmail.com>
2023-09-22 10:13:05 -0500

    archivo que contendra los textos en los distintos idiomas

---
+++ language/home.php
@@ -0,0 +1,21 @@
+<?php
+$texto = [
+    "es" =>
+    [
+        "header" => [
+            "Dónde ir",
+            "Agenda de Fiesta",
+            "Galeria Turística",
+            "Planifica tu viaje",
+        ]
+    ],
+    "en" =>
+    [
+        "header" => [
+            "Where to Go",
+            "Party Agenda",
+            "Tourist Gallery",
+            "Plan your Trip",
+        ]
+    ]
+];
