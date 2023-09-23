8375dbf59c1e0ee092ac415fd26f868cd7eaf175
EliezerSu <eliezersuarez2000@gmail.com>
2023-09-22 21:37:19 -0500

    añado ejemplo de la entrada

---
+++ language/la_entrada.php
@@ -0,0 +1,33 @@
+<?php
+$texto = [
+    "es" =>
+    [
+        "header" => [
+            "Acerca de",
+            "¿Qué visitar?",
+            "¿Qué hacer?",
+            "Planifica tu viaje",
+            "subtitle-1" => [
+                "Culturas y tradiciones",
+                "Gastronomía"
+            ]
+        ],
+        "body" => ["Es un pueblo dedicado a la pesca y al comercio de sus productos como principales platos típicos que venden para el degustar de los que visitan la comuna La Entrada y venta de principales productos del diario vivir, sus casas pintorescas y llamativas hacen este lugar un lugar de cultura y visita turística."]
+    ],
+    "en" =>
+    [
+        "header" => [
+            "About",
+            "What to visit?",
+            "To do?",
+            "Plan your Trip",
+            "subtitle-1" => [
+                "Cultures and traditions",
+                "Gastronomy"
+            ]
+        ],
+        "body" => [
+            "It is a town dedicated to fishing and the trade of its products such as the main typical dishes that they sell for the taste of those who visit the commune La Entry and sale of main products of daily life, its picturesque and striking houses make this place a place of culture and tourist visit."
+        ]
+    ]
+];
